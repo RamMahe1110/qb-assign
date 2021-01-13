ce78c6fd5879bb03929635573446bf517246bb32
Ram Maheshwari <ram991011@gmail.com>
2021-01-13 23:55:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import Premium from './pages/Premium'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="main-nav">
+        <div className="content">
+          <div className="brand-logo-cont">
+            <img
+              src="https://static.grammarly.com/assets/files/19fc696d4cf13c26d65fceffd734afff/grammarly_logo.svg"
+              alt="brand-name"
+            />
+          </div>
+          <div className="right-cont">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://grammarly.com"
+              className="text-sec"
+            >
+              Log In
+            </a>
+            <button className="btn btn-sm btn-nav">Upgrade to Premium</button>
+          </div>
+        </div>
+      </nav>
+      <Premium />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Premium.jsx
@@ -0,0 +1,118 @@
+import React, { Component } from 'react'
+
+class Premium extends Component {
+  state = {}
+  render() {
+    return (
+      <div className="premium">
+        <section className="hero-section">
+          <div className="sticky-info-card">
+            <div className="dots">
+              <span className="dot">.</span>
+              <span className="dot">.</span>
+              <span className="dot">.</span>
+            </div>
+            <p className="text">Hi Denise,</p>
+            <p className="text">
+              Now that I've had time to consider your requirements thoughtfully,
+              please find attached a tentative timeline for the project.
+            </p>
+            <p className="text">
+              If you have any questions about my rate or if you find it
+              necessary to increase or decrease the scope for this project,
+              please let me know.
+            </p>
+            <div className="bar">
+              <div className="percent-bar">.</div>
+              <span className="val">69</span>
+            </div>
+          </div>
+          <div className="single-hero-feature">
+            <div className="container single-hero-cont">
+              <div className="indi-spec">
+                <h2 className="heading heading-sec heading-indi-spec">
+                  When your writing stands out, so do you.
+                </h2>
+                <p className="text-prim desc">
+                  Turn writing that works into writing that gets results with
+                  Grammarly Premium.
+                </p>
+                <button className="btn">Upgrade to Premium</button>
+              </div>
+            </div>
+          </div>
+          <div className="single-hero-feature">
+            <div className="container single-hero-cont">
+              <div className="indi-spec">
+                <div className="short-title-cont">
+                  <span className="icon-cont">
+                    <img
+                      src="https://static.grammarly.com/assets/files/5870dbd72743746761c17ea7ed8fd971/ic-clarity.svg"
+                      alt="icon"
+                    />
+                  </span>
+                  <h4 className="short-title text-sec">Clarity</h4>
+                </div>
+                <h2 className="heading heading-sec heading-indi-spec">
+                  When your writing stands out, so do you.
+                </h2>
+                <p className="text-prim desc">
+                  Turn writing that works into writing that gets results with
+                  Grammarly Premium.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="single-hero-feature">
+            <div className="container single-hero-cont">
+              <div className="indi-spec">
+                <div className="short-title-cont">
+                  <span className="icon-cont">
+                    <img
+                      src="https://static.grammarly.com/assets/files/5870dbd72743746761c17ea7ed8fd971/ic-clarity.svg"
+                      alt="icon"
+                    />
+                  </span>
+                  <h4 className="short-title text-sec">Clarity</h4>
+                </div>
+                <h2 className="heading heading-sec heading-indi-spec">
+                  When your writing stands out, so do you.
+                </h2>
+                <p className="text-prim desc">
+                  Turn writing that works into writing that gets results with
+                  Grammarly Premium.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="single-hero-feature">
+            <div className="container single-hero-cont">
+              <div className="indi-spec">
+                <div className="short-title-cont">
+                  <span className="icon-cont">
+                    <img
+                      src="https://static.grammarly.com/assets/files/5870dbd72743746761c17ea7ed8fd971/ic-clarity.svg"
+                      alt="icon"
+                    />
+                  </span>
+                  <h4 className="short-title text-sec">Clarity</h4>
+                </div>
+                <h2 className="heading heading-sec heading-indi-spec">
+                  When your writing stands out, so do you.
+                </h2>
+                <p className="text-prim desc">
+                  Turn writing that works into writing that gets results with
+                  Grammarly Premium.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="random">hhs</section>
+      </div>
+    )
+  }
+}
+
+export default Premium
